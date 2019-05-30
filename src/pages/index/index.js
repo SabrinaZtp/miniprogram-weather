38f60876fe2a9ac1e1261effd2291c611965406c
Sabrina <klaylazh@gmail.com>
2019-05-30 17:51:37 +0800

    Send http request and enable pull-down refresh

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,63 @@
-Page({})
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+Page({
+  data: {
+    nowTemp: '',
+    nowWeather: '',
+    nowWeatherBackground: ''
+  },
+  onPullDownRefresh() {
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+    });
+  },
+  onLoad() {
+    this.getNow();
+  },
+  getNow(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '北京市'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: (res) => {
+        let result = res.data.result;
+        let weather = result.now.weather;
+        let temp = result.now.temp;
+        console.log(weather + " " + temp);
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png'
+        });
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        });
+      },
+      complete: () => {
+        callback && callback();
+      }
+    })
+  }
+  
+})
